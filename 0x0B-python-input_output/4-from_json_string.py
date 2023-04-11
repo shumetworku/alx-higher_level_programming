0931adb4d756b79e0ef424d88c1a6a362e4b8f36
shumetworku <shumwor@gmail.com>
2023-04-11 07:07:03 -0700

    python input output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A JSON script
+© 2022 baccrie
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Will update later
+    """
+    new = json.loads(my_str)
+    return (new)
