1ed12d9ea1c1779c28c0024aca609021f7c7f6ca
shumetworku <shumwor@gmail.com>
2023-04-10 23:15:51 -0700

    python input output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+"""A module that does wonder"""
+
+
+def read_file(filename=""):
+    """Function opens a file for.reading"""
+    with open(filename, 'r',  encoding="utf-8") as f:
+        for i in f:
+            print(i, end='')
