01faf706f29c0123632fdc267bca74afca21df75
shumetworku <shumwor@gmail.com>
2023-04-11 23:29:02 -0700

    python input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""
+Module 7-add_item
+"""
 
 
-import json
+from sys import argv
 
+s = __import__('5-save_to_json_file').save_to_json_file
+m = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+try:
+    files_before = m('add_item.json')
+except FileNotFoundError:
+    files_before = []
+
+lists = files_before + argv[1:]
+
+s(lists, 'add_item.json')
