6044316b1f6fa741aa7adac613533fc2a961f557
shumetworku <shumwor@gmail.com>
2023-04-11 23:42:58 -0700

    python input output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python3
-'''task 10 module'''
+"""
+    Defines a student class
+"""
 
 
 class Student:
-    '''student class'''
+    """
+        Defines a student class
+    """
+
     def __init__(self, first_name, last_name, age):
-        '''Initialization of the student object'''
+        """
+            Initializing instance variables
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        '''return a dictionary representation of a Student instance
-        If attrs is a list of strings, only attribute names contained
-        in this list must be retrieved.
-        Otherwise, all attributes must be retrieved
-        '''
-        try:
-            for attr in attrs:
-                if type(attr) is not str:
-                    return self.__dict__
-        except Exception:
+        """
+            Retrieves the dict reprenting the class
+        """
+        if attrs is None:
             return self.__dict__
-        my_dict = dict()
-        for key, value in self.__dict__.items():
-            if key in attrs:
-                my_dict[key] = value
-        return 
+        my_dict = {}
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                my_dict[attr] = self.__dict__[attr]
+        return my_dict
